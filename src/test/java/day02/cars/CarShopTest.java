8844e01037f64248652594b844fcf7475c42d862
Márton András Hábele <marton.habele@gmail.com>
2021-11-19 17:11:28 +0100

    Day 02 tasks updated

--- src/test/java/day02/cars/CarShopTest.java
+++ src/test/java/day02/cars/CarShopTest.java
@@ -1,5 +1,6 @@
 package day02.cars;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -7,18 +8,22 @@ import static org.junit.jupiter.api.Assertions.*;
 class CarShopTest {
     CarShop carShop = new CarShop("TripleAAutos", 100);
 
+    @BeforeEach
+    void init() {
+        carShop.addCar(new Car("Hyundai", 1.4, Color.GREY, 100));
+        carShop.addCar(new Car("KIA", 2.2, Color.BLACK, 101));
+    }
+
     @Test
     void createCarShopTest() {
 
         assertEquals("TripleAAutos", carShop.getName());
         assertEquals(100, carShop.getLimitPriceMax());
-        assertEquals(0, carShop.getCarsForSell().size());
+        assertEquals(1, carShop.getCarsForSell().size());
     }
 
     @Test
     void addCarWithPrices() {
-        assertTrue(carShop.addCar(new Car("Hyundai", 1.4, Color.GREY, 100)));
-        assertFalse(carShop.addCar(new Car("KIA", 2.2, Color.BLACK, 101)));
         assertEquals(1, carShop.getCarsForSell().size());
         assertEquals("Hyundai", carShop.getCarsForSell().get(0).getBrand());
     }
